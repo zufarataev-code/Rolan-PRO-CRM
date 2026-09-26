e3622d6c58663a7b54782e8e4b20c673878c4af5
zufarataev-code <zufar.ataev@icloud.com>
2026-09-25 23:08:02 -0700

    chore(calendar): remove unused stub month renderer

--- src/features/legacy-crm/renderDispatchMonthMobile.ts
+++
@@ -1,6 +0,0 @@
-import { renderMobileCalendarAgenda } from './RenderMobileCalendarAgenda';
-
-export function renderDispatchMonthMobile(anchor) {
-  // Имплементация мобильного представления месяца с компактной сеткой и индикатором событий
-}
-
